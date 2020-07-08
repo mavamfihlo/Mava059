795b2a5dfc51c4811e977e507a1d29cf5ef73c36
mavamfihlo <61974448+mavamfihlo@users.noreply.github.com>
2020-07-08 16:41:51 +0200

    Add files via upload

---
+++ DataFrame_Sprint5.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+import pandas as pd
+
+stock_data = {
+
+    
+    'Chips' : ['Simba','Lays','None'],
+    'Cooldrinks' : ['Coke','Fanta','None'],
+    'Chocolate' : ['Cadbury','Tex','None'],  
+    'Pie' : ['Pepper Steak','Chicken','None'],
+    'Fruit' : ['Pear','Apple','Orange'],
+    'Cupcake' : ['Vanilla','Chocolate','None'],
+    'Veggies' : ['Spinach','Cabbage','None']
+    }
+
+df = pd.DataFrame(stock_data)
+df
+
+
+# In[ ]:
+
+
+
+
